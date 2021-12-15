ccf816c4b5ab043dee02529ac79f30e1a3844a00
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-15 07:59:23 -0700

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect ,Link} from 'react-router-dom';
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
+import Logout from './components/Logout';
+import PrivateRoute from './components/PrivateRoute';
+
 function App() {
   return (
     
     <Router>
     <div className="App">
-      <Route path ="/">
+      <header>
+        <h2>Friends</h2>
+        <Link className='link' to='/login'>Login</Link>
+        <Link className='link' to='/friends'> Friends List</Link>
+        <Link className='link' to='/friends/add'>Add a friend</Link>
+        <Link className='link' to='/logout'>Logout</Link>
+
+        </header>
+
+      <Route exact path ="/">
         <Login/>
+      </Route>
+          
+        <Route exact path ="/login">
+          <Redirect to="/"/>
           </Route>
-          <Route path ="/login">
-        <Login/>
-          </Route>
-          <Route path ="/friends">
-        <FriendsList/>
-          </Route>
+          <PrivateRoute path="/friends" component={FriendsList}/>
+      
           <Route path ="/friends/add">
         <AddFriend/>
+          </Route>
+          <Route path ="/logout">
+        <Logout/>
           </Route>
           </div>
     </Router>
--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,21 +1,52 @@
-import React from "react";
-
-const AddFriend = (props)=>{
-    const {friend} = props;
+import React, {useState} from "react";
+import { useHistory } from "react-router";
+import axios
+ from "axios";
+const AddFriend = () => {
+    const {push} = useHistory();
+    const [friend, setFriend] = useState({
+        name: '',
+        age: '',
+        email: ''
+    })
+    const handleChange =(e) => {
+        setFriend({
+            ...friend,
+            [e.target.name]:e.target.value
+        });
+        
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            const token = localStorage.getItem('token')
+            return( 
+                axios.post(`http://localhost:9000/api/friends`, friend, {
+                    headers: {
+                        authorization: token
+                    }
+                })
+                .then(res => {
+                    push('/friends');
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+            );
+            }
+    }
     return(<div>
         <h2>Add Friend</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="username">Username:</label>
-                <input id="username"/>
+                <label htmlFor="name">Name:</label>
+                <input onChange={handleChange} id="name"/>
             </div>
             <div>
                 <label htmlFor="age">Age:</label>
-                <input id="age"/>
+                <input onChange={handleChange} id="age"/>
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input id="email"/>
+                <input onChange={handleChange} id="email"/>
             </div>
             <button>Submit</button>
         </form>
